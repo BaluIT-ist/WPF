7e47eec6f71ddb1e5da7b3f252172461af56a6cb
dgadrian05 <dgadrian05@gmail.com>
2023-09-18 16:39:33 +0300

    Created Project

---
+++ WPF LEARNING/WPF LEARNING/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace WPF_LEARNING
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            tbHello.Text = "Hello World 2";
+        }
+
+        private void btnRun_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
