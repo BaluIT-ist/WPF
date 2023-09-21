7d332f05f2c2fd253bf17e5fbca5ae0611c5d6fa
dgadrian05 <dgadrian05@gmail.com>
2023-09-21 12:54:06 +0300

    Created Project  No#4 Created basic Form

---
+++ WPF Learning 4/WpfApp1/WpfApp1/View/UserControls/ClearableTextBox.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1.View.UserControls
+{
+  
+    public partial class ClearableTextBox : UserControl
+    {
+        public ClearableTextBox()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private string placeholder;
+
+        public string PlaceHolder
+        {
+            get { return placeholder; }
+            set 
+            {
+                placeholder = value;
+                tbhPlaceholder.Text = placeholder;  
+            }
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtInput.Clear();
+            txtInput.Focus();
+        }
+
+        private void txtInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtInput.Text))
+                tbhPlaceholder.Visibility = Visibility.Visible;
+            else
+                tbhPlaceholder.Visibility= Visibility.Hidden;
+        }
+    }
+}
