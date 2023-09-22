c57395d1a4cf38b56ff3a9989e380710a9c24924
dgadrian05 <dgadrian05@gmail.com>
2023-09-22 15:11:47 +0300

    Created Project

---
+++ WPF Learning 4/WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WpfApp2
+{
+
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        private string boundtext;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string BoundText
+        {
+            get { return boundtext; }
+            set
+            {
+                boundtext = value;
+                OnProperyChanged();
+
+            }
+        }
+
+        private void btnSet_Click(object sender, RoutedEventArgs e)
+        {
+            BoundText = "set from code";
+        }
+
+        private void OnProperyChanged([CallerMemberName] string propertyName=null)
+        {
+            PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
